d3a4cc8d61dc4e54cc0fe5b4a4a9bb4cf8f542c6
spelltomato <spelltomato@gmail.com>
2025-01-04 19:25:17 -0600

    library porject addded

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./projects.css";
 import resumeGod from "../assets/resumeGod.png";
 import movie from "../assets/movie.png";
+import libraryCover from "../assets/libraryCover.png";
 
 function Projects() {
   return (
@@ -39,7 +40,59 @@ function Projects() {
                     </p>
                     <div className="projects__description--links">
                       <a
+                        target="_blank"
                         href="https://resumegodai-two.vercel.app/"
+                        className="project__description--link"
+                      >
+                        <i
+                          className="fas fa-link project__social--link"
+                          aria-hidden="true"
+                        ></i>
+                      </a>
+                      {/* <a
+                        href="https://github.com/scottdialo"
+                        className="project__description--link"
+                      >
+                        <i
+                          className="fab fa-github project__social--link"
+                          aria-hidden="true"
+                        ></i>
+                      </a> */}
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li className="project" id="project">
+                <div className="project__wrapper">
+                  <img
+                    src={libraryCover}
+                    alt="resume God mockup img"
+                    className="project__img"
+                  />
+                  <div className="project__wrapper--background"></div>
+                  <div
+                    className="project__description"
+                    id="project__description"
+                  >
+                    <h3 className="project__description--title">
+                      Easy to use website to find the perfect book to read -
+                      ATx-Library
+                    </h3>
+                    <h4 className="project__description--sub-title">
+                      Html, CSS, JavaScript
+                    </h4>
+                    <p className="project__description--para">
+                      Discover your next great read with Atx-library. Our
+                      intuitive library platform lets you easily filter and
+                      explore a wide selection of books, from highly rated to
+                      hidden gems. Whether you're looking to expand your
+                      knowledge or escape into a new story, Atx-library is your
+                      go-to resource for finding the perfect book.
+                    </p>
+                    <div className="projects__description--links">
+                      <a
+                        target="_blank"
+                        href="https://atx-library.vercel.app"
                         className="project__description--link"
                       >
                         <i
@@ -82,6 +135,7 @@ function Projects() {
                     </p>
                     <div className="projects__description--links">
                       <a
+                        target="_blank"
                         href="https://kahirde.com/"
                         className="project__description--link"
                       >
